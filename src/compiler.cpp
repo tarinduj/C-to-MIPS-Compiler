d2b480d40f6fe49a9abd028dd7fe44f996e04373
Adam Urban <adamurban98@gmail.com>
2019-03-10 18:24:38 +0000

    Formatting

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,8 +1,5 @@
-#include <iostream>
 #include "compiler.hpp"
 #include "run.hpp"
+#include <iostream>
 
-int main(int argc, char const *argv[])
-{
-	return run(argc, argv);
-}
+int main(int argc, char const *argv[]) { return run(argc, argv); }
