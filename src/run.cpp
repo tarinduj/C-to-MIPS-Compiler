5848d5ad977676d7336f87140be27f31a841246e
Adam Urban <adamurban98@gmail.com>
2019-03-19 12:27:40 +0000

    Add logger_macros

--- src/run.cpp
+++ src/run.cpp
@@ -6,28 +6,13 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include "logger_macros.hpp"
+
 
 int scopeCounter = 0;
 std::vector<std::string> globalVarNames;
 bool elif = 0;
 
-void msg(std::string msg, bool endl) {
-  if (verbosity_config == V_NORM || verbosity_config == V_HIGH) {
-    std::cerr << msg;
-    if (endl) {
-      std::cerr << '\n';
-    }
-  }
-}
-
-void log(std::string msg, bool endl) {
-  if (verbosity_config == V_HIGH) {
-    std::cerr << msg;
-    if (endl) {
-      std::cerr << '\n';
-    }
-  }
-}
 
 int run(int argc, char const *argv[]) {
   bool compile = false;
@@ -36,26 +21,11 @@ int run(int argc, char const *argv[]) {
   std::string output_file;
   std::string input_file;
 
-  auto const setVerbosity = [&](std::string const &verbosity_str) {
-    if (verbosity_str == "quiet" || verbosity_str == "q")
-      verbosity_config = V_QUIET;
-    else if (verbosity_str == "normal" || verbosity_str == "n")
-      verbosity_config = V_NORM;
-    else if (verbosity_str == "high" || verbosity_str == "h")
-      verbosity_config = V_HIGH;
-    else
-      return clara::ParserResult::runtimeError("Unrecognised verbosity, '" +
-                                               verbosity_str + "'");
-    return clara::ParserResult::ok(clara::ParseResultType::Matched);
-  };
-
   auto cli =
       clara::Help(help) |
       clara::Opt(compile)["-c"]["--compile"]("compile from C98 to MIPS") |
       clara::Opt(translate)["-t"]["--translate"](
           "translate from C89 to Python") |
-      clara::Opt(setVerbosity, "quiet|normal|high")["-v"]["--verbosity"](
-          "set verbosity of messages") |
       clara::Opt(output_file, "output")["-o"]("place the output into <file>") |
       clara::Arg(input_file, "input")("Input file").required();
 
@@ -64,11 +34,9 @@ int run(int argc, char const *argv[]) {
   if (help) {
     std::cout << cli;
   } else if (compile) {
-    msg(fmt::format("I am gonna compile from {} to {}.", input_file,
-                    output_file));
+    MSG << fmt::format("I am gonna compile from {} to {}\n", input_file, output_file);
   } else if (translate) {
-    msg(fmt::format("I am gonna translate from {} to {}.", input_file,
-                    output_file));
+    MSG << fmt::format("I am gonna translate from {} to {}\n", input_file, output_file);
     std::ofstream target_file(output_file);
     NodePtr ast = parseAST(input_file);
     ast->pyPrint(target_file);
@@ -80,6 +48,7 @@ int run(int argc, char const *argv[]) {
     }
   }
 
+  MSG << "Finished\n";
   return 0;
 }
 
