5e6c99c7459a6862fb25f6800d746f31604d7fc4
Adam Urban <adamurban98@gmail.com>
2019-03-22 11:29:33 +0000

    Fix comment style

--- test_deliverable/test_cases/first_test.c
+++ test_deliverable/test_cases/first_test.c
@@ -1,8 +1,7 @@
 int foo(){
   int a = 6, b, c = 072, d;
-
-  c += a; //c = 64
-  b = a; //b = 6
-  d = c + a + 5; //d = 75
+  c += a; /* c = 64 */
+  b = a; /* b = 6 */
+  d = c + a + 5; /* d = 75 */
   return d;
 }
