55cc14843934c7a6289b9cd1c5d8bf0dadb4d0a3
Adam Urban <adamurban98@gmail.com>
2019-03-22 23:29:27 +0000

    Fix operators test

--- test_deliverable/test_cases/operators.c
+++ test_deliverable/test_cases/operators.c
@@ -29,43 +29,43 @@ int bxor(int a, int b){
 	return a ^ b;
 }
 int assign_add(int a, int b){
-	a =+ b;
+	a += b;
 	return a;
 }
 int assign_sub(int a, int b){
-	a =- b;
+	a -= b;
 	return a;
 }
 int assign_mul(int a, int b){
-	a =* b;
+	a *= b;
 	return a;
 }
 int assign_div(int a, int b){
-	a =/ b;
+	a /= b;
 	return a;
 }
 int assign_mod(int a, int b){
-	a =% b;
+	a %= b;
 	return a;
 }
 int assign_shr(int a, int b){
-	a =>> b;
+	a >>= b;
 	return a;
 }
 int assign_shl(int a, int b){
-	a =<< b;
+	a <<= b;
 	return a;
 }
 int assign_band(int a, int b){
-	a =& b;
+	a &= b;
 	return a;
 }
 int assign_bor(int a, int b){
-	a =| b;
+	a |= b;
 	return a;
 }
 int assign_bxor(int a, int b){
-	a =^ b;
+	a ^= b;
 	return a;
 }
 
