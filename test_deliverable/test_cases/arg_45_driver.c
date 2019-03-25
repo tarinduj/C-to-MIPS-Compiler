fdd9d72282e0b4781a05449f1dedfbe1ba4a9579
Adam Urban <adamurban98@gmail.com>
2019-03-25 20:13:17 +0000

    Add test for 4 5 6 arguments

---
+++ test_deliverable/test_cases/arg_45_driver.c
@@ -0,0 +1,20 @@
+int four(int a, int b, int c, int d);
+int five(int a, int b, int c, int d, int e);
+int six(int a, int b, int c, int d, int e, int f);
+
+int main(){
+	int a = 1;
+	int b = 2;
+	int c = 3;
+	int d = 4;
+	int e = 5;
+	int f = 6;
+	if ( four(1,2,3,4) != 4 ) {
+		return 1;
+	} else if ( five(1,2,3,4,5) != 5 ) {
+		return 2;
+	} else if ( six(1,2,3,4,5,6) != 6 ) {
+		return 3;
+	}
+	return 0;
+}
