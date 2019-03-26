b4f7b8089d6d1375db8ce1feb7d03e4461fc4203
Adam Urban <adamurban98@gmail.com>
2019-03-26 12:30:26 +0000

    Modify test

--- test_deliverable/test_cases/call_5_arguments.c
+++ test_deliverable/test_cases/call_5_arguments.c
@@ -1,8 +1,55 @@
-int global_variable = 5;
-int bar(int a, int b, int c, int d, int e, int f, int g){
-  return e;
+int get_a(int a, int b, int c, int d, int e, int f, int g){
+	return a;
 }
-int foo(){
-  int x = bar(7, 2, 11, 12, 4, 5, 6);
-  return x;
+
+int call_get_a(){
+	return get_a(1,2,3,4,5,6,7);
+} 
+
+int get_b(int a, int b, int c, int d, int e, int f, int g){
+	return b;
+}
+
+int call_get_b(){
+	return get_b(1,2,3,4,5,6,7);
+} 
+
+int get_c(int a, int b, int c, int d, int e, int f, int g){
+	return c;
+}
+
+int call_get_c(){
+	return get_c(1,2,3,4,5,6,7);
+} 
+
+int get_d(int a, int b, int c, int d, int e, int f, int g){
+	return d;
+}
+
+int call_get_d(){
+	return get_d(1,2,3,4,5,6,7);
+} 
+
+int get_e(int a, int b, int c, int d, int e, int f, int g){
+	return e;
+}
+
+int call_get_e(){
+	return get_e(1,2,3,4,5,6,7);
+} 
+
+int get_f(int a, int b, int c, int d, int e, int f, int g){
+	return f;
+}
+
+int call_get_f(){
+	return get_f(1,2,3,4,5,6,7);
+} 
+
+int get_g(int a, int b, int c, int d, int e, int f, int g){
+	return g;
+}
+
+int call_get_g(){
+	return get_g(1,2,3,4,5,6,7);
 }
--- test_deliverable/test_cases/call_5_arguments_driver.c
+++ test_deliverable/test_cases/call_5_arguments_driver.c
@@ -1,4 +1,36 @@
-int foo();
+/*#include <stdio.h>*/
+
+int call_get_a();
+int call_get_b();
+int call_get_c();
+int call_get_d();
+int call_get_e();
+int call_get_f();
+int call_get_g();
+
 int main(){
-  return foo() != 4;
+ 	/*printf("Arguments got back in order: %d %d %d %d %d %d %d. ", call_get_a(), call_get_b(), call_get_c(), call_get_d(), call_get_e(), call_get_f(), call_get_g()); */
+ 	if(call_get_a() != 1){
+		return 1;
+	} 
+	else if(call_get_b() != 2){
+		return 2;
+	} 
+	else if(call_get_c() != 3){
+		return 3;
+	} 
+	else if(call_get_d() != 4){
+		return 4;
+	} 
+	else if(call_get_e() != 5){
+		return 5;
+	} 
+	else if(call_get_f() != 6){
+		return 6;
+	} 
+	else if(call_get_g() != 7){
+		return 7;
+	} else {
+		return 0;
+	}
 }
