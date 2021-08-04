00a416ca3ff5c28d887271f39e639f0393e20cf6
Alastair F. Donaldson <alastair.donaldson@imperial.ac.uk>
2021-08-04 14:48:54 +0100

    CMake target for fuzzer.

---
+++ fuzzer/fuzzer.cpp
@@ -0,0 +1,8 @@
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  const char* input_file = "_temp.c";
+  const char* args[] = { "c-to-mips", input_file, "-o", "_temp.mps", "-c" };
+
+  // TODO: write `size` bytes of `data` to a file named `input_file`,
+  //  then invoke the `run` method of the compile with `args`
+  return 0;
+}
