cbad2a267379396ce08228decdd08443a69f3436
Alastair F. Donaldson <alastair.donaldson@imperial.ac.uk>
2021-08-05 14:31:19 +0100

    Add missing includes.

--- fuzzer/fuzzer.cpp
+++ fuzzer/fuzzer.cpp
@@ -1,3 +1,6 @@
+#include <cinttypes>
+#include <cstddef>
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   const char* input_file = "_temp.c";
   const char* args[] = { "c-to-mips", input_file, "-o", "_temp.mps", "-c" };
